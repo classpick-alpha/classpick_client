51f496bf53581c2c2f66ebb11c01c6955c3364f6
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-06 14:40:36 +0900

    fix(room-filter): 사이드바 필터링을 메인 페이지(타임테이블)에서만 보이도록 수정

--- src/components/side-bar/index.tsx
+++ src/components/side-bar/index.tsx
@@ -1,18 +1,26 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 import SideBarFilter from '@/components/side-bar/filter';
 import SideBarMenu from '@/components/side-bar/menu';
 
 export default function SideBar() {
+  const pathname = usePathname();
+
   return (
     <div className="h-[calc(100dvh-80px-32px)] min-w-[280px] rounded-2xl bg-white">
       <SideBarMenu />
 
-      <hr className="border-1 border-gray-100" />
+      {pathname === '/' && (
+        <>
+          <hr className="border-1 border-gray-100" />
 
-      <div className="my-2 max-h-[calc(100dvh-80px-32px-156px-16px)] overflow-y-auto">
-        <SideBarFilter />
-      </div>
+          <div className="my-2 max-h-[calc(100dvh-80px-32px-156px-16px)] overflow-y-auto">
+            <SideBarFilter />
+          </div>
+        </>
+      )}
     </div>
   );
 }
