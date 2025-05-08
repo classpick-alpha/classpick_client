994fb6de1130c17ec23fa10afd29b7f9bf77e7bd
Son Daehyeon <sondaehyeon01@gmail.com>
2025-05-08 16:30:33 +0900

    fix(modal): 모달을 끌 수 있도록 수정

--- src/components/modal-container/index.tsx
+++ src/components/modal-container/index.tsx
@@ -1,15 +1,39 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import { useModalStore } from '@/store/modal.store';
+import { X } from 'lucide-react';
 
 export default function ModalContainer() {
-  const { modal } = useModalStore();
+  const { modal, closeModal } = useModalStore();
+
+  useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    };
+
+    window.addEventListener('keydown', onKeyDown);
+    return () => window.removeEventListener('keydown', onKeyDown);
+  }, [closeModal]);
 
   if (!modal) return null;
 
   return (
-    <div className="absolute z-99 h-full w-full bg-black/50">
-      <div className="relative top-1/2 left-1/2 w-fit -translate-x-1/2 -translate-y-1/2 overflow-hidden rounded-3xl bg-white">
+    <div className="fixed inset-0 z-90 bg-black/50" onClick={closeModal}>
+      <div
+        className="relative top-1/2 left-1/2 max-h-[90dvh] w-fit -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-3xl bg-white"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="text-primary-gray-500 absolute top-4 right-4 z-[99] cursor-pointer rounded-full p-0.5"
+          onClick={closeModal}
+        >
+          <X size={20} />
+        </button>
+
         {modal}
       </div>
     </div>
