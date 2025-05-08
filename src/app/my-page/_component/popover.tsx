88895f513b64d3cd219eafc87f4a78a24cb71358
Son Daehyeon <sondaehyeon01@gmail.com>
2025-05-09 05:59:58 +0900

    [CP-206] feat: 현재 예약 현황(칸반형) 퍼블리싱

---
+++ src/app/my-page/_component/popover.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+import { CircleCheck } from 'lucide-react';
+import { twMerge } from 'tailwind-merge';
+
+export function Popover() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="relative flex-1"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        className="size-6 cursor-pointer"
+      >
+        <path
+          d="M2 12.9385C2 8.22443 2 5.86741 3.46447 4.40294C4.92893 2.93848 7.28595 2.93848 12 2.93848C16.714 2.93848 19.0711 2.93848 20.5355 4.40294C22 5.86741 22 8.22443 22 12.9385C22 17.6525 22 20.0095 20.5355 21.474C19.0711 22.9385 16.714 22.9385 12 22.9385C7.28595 22.9385 4.92893 22.9385 3.46447 21.474C2 20.0095 2 17.6525 2 12.9385Z"
+          fill="#3C3B41"
+          stroke="#3C3B41"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M10.125 9.81348C10.125 8.77794 10.9645 7.93848 12 7.93848C13.0355 7.93848 13.875 8.77794 13.875 9.81348C13.875 10.5009 13.505 11.102 12.9534 11.4284C12.478 11.7096 12 12.1362 12 12.6885V13.9385"
+          stroke="white"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+        />
+        <circle cx="12" cy="16.9385" r="1" fill="white" />
+      </svg>
+      <div
+        className={twMerge(
+          'absolute bottom-0 left-8 z-20 flex w-fit translate-y-1/2 flex-col gap-3 rounded-lg bg-gray-800 px-2 py-3 text-sm text-white',
+          !isHovered && 'hidden',
+        )}
+      >
+        <p className="body2-nanum text-white">나의 예약 현황을 한번에 확인해요</p>
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-1">
+            <CircleCheck size={14} />
+            <p className="caption2-nanum text-white">예약반려 사유 확인 후, 추가 서류제출</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <CircleCheck size={14} />
+            <p className="caption2-nanum text-white">강의실이 사용한 후, 인증사진 업로드</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className={twMerge(
+          'absolute top-1 left-7 z-10 size-4 rotate-45 bg-gray-800',
+          !isHovered && 'hidden',
+        )}
+      />
+    </div>
+  );
+}
--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,3 +1,232 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import InfoCard from '@/app/my-page/_component/info-card';
+import { Popover } from '@/app/my-page/_component/popover';
+
+import EmptyReservationsImage from '@/public/my-page/empty-reservations.png';
+
+import Api from '@/api';
+import { ReservationResponse, Status } from '@/api/dto/reservation';
+import { useApi } from '@/hook/use-api';
+import { now } from '@/util';
+import { differenceInDays, format, isBefore, parse } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { Kanban } from 'iconsax-react';
+import { LayoutGrid } from 'lucide-react';
+import { twMerge } from 'tailwind-merge';
+
+type LayoutType = 'kanban' | 'grid';
+
+const steps = [
+  {
+    title: '예약심사',
+    textColor: 'text-primary-gray-300',
+    bgColor: 'bg-primary-gray-300',
+    borderColor: 'hover:border-primary-gray-300',
+    filter: (reservation: ReservationResponse) => reservation.status === Status.REQUESTED,
+    renderBadge: () => (
+      <p className="text-primary-gray-500 body2-pretendard h-fit rounded bg-zinc-100 px-2 py-0.5">
+        예약심사
+      </p>
+    ),
+  },
+  {
+    title: '예약반려',
+    textColor: 'text-system-alarm',
+    bgColor: 'bg-system-alarm',
+    borderColor: 'hover:border-system-alarm',
+    filter: (reservation: ReservationResponse) => reservation.status === Status.REJECTED,
+    renderBadge: () => (
+      <p className="bg-system-alarm/10 text-system-alarm body2-pretendard h-fit rounded px-2 py-0.5">
+        예약심사
+      </p>
+    ),
+  },
+  {
+    title: '예약완료',
+    textColor: 'text-classpick-300',
+    bgColor: 'bg-classpick-300',
+    borderColor: 'hover:border-classpick-300',
+    filter: (reservation: ReservationResponse) =>
+      reservation.status === Status.APPROVED &&
+      isBefore(
+        now(),
+        parse(reservation.endTime, 'HH:mm', parse(reservation.date, 'yyyy-MM-dd', new Date())),
+      ),
+    renderBadge: () => (
+      <p className="bg-classpick-400/10 text-classpick-400 body2-pretendard h-fit rounded px-2 py-0.5">
+        예약심사
+      </p>
+    ),
+  },
+  {
+    title: '이용완료',
+    textColor: 'text-emerald-400',
+    bgColor: 'bg-emerald-400',
+    borderColor: 'hover:border-emerald-400',
+    filter: (reservation: ReservationResponse) =>
+      reservation.status === Status.APPROVED &&
+      !isBefore(
+        now(),
+        parse(reservation.endTime, 'HH:mm', parse(reservation.date, 'yyyy-MM-dd', new Date())),
+      ),
+    renderBadge: (reservation: ReservationResponse) =>
+      reservation.people === 1 ? (
+        <p className="body2-pretendard h-fit rounded bg-emerald-400/10 px-2 py-0.5 text-emerald-400">
+          인증필요
+        </p>
+      ) : (
+        <p className="text-primary-gray-500 body2-pretendard h-fit rounded bg-zinc-100 px-2 py-0.5">
+          인증완료
+        </p>
+      ),
+  },
+];
+
 export default function Page() {
-  return <div>Page Section</div>;
+  const [isApiProcessing, startApi] = useApi();
+
+  const [reservations, setReservations] = useState<ReservationResponse[]>([]);
+  const [layoutType, setLayoutType] = useState<LayoutType>('kanban');
+
+  useEffect(() => {
+    startApi(async () => {
+      const { reservations } = await Api.Domain.Reservation.getReservationsList();
+      setReservations(reservations);
+    });
+  }, []);
+
+  if (isApiProcessing) return null;
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <InfoCard reservations={reservations} />
+
+      <div className="flex h-full flex-col gap-8 rounded-2xl bg-white p-8 pb-4">
+        <div className="flex justify-between">
+          <div className="flex w-full flex-col gap-2">
+            <div className="flex items-center gap-2">
+              <h2 className="text-primary-gray-600 title2-nanum font-bold">현재 예약 현황</h2>
+              <Popover />
+            </div>
+            <p className="caption1-nanum">현재까지 예약된 강의실의 신청 현황입니다.</p>
+          </div>
+
+          <div className="flex gap-2">
+            <LayoutGrid
+              size={24}
+              className="cursor-pointer"
+              color={
+                layoutType === 'grid' ? 'var(--color-classpick-500)' : 'var(--color-classpick-100)'
+              }
+              onClick={() => setLayoutType('grid')}
+            />
+            <Kanban
+              size={24}
+              className="cursor-pointer"
+              color={
+                layoutType === 'kanban'
+                  ? 'var(--color-classpick-500)'
+                  : 'var(--color-classpick-100)'
+              }
+              onClick={() => setLayoutType('kanban')}
+            />
+          </div>
+        </div>
+
+        {layoutType === 'kanban' ? (
+          <>
+            <div className="grid grid-cols-4 gap-5">
+              {steps.map((step) => (
+                <div key={step.title} className="flex flex-col gap-2.5">
+                  <p className={twMerge('caption1-nanum font-bold', step.textColor)}>
+                    {step.title}
+                  </p>
+                  <div className={twMerge('h-[6px] w-full rounded-full', step.bgColor)} />
+                  <div className="flex max-h-[calc(100dvh-80px-180px-52px-166px)] flex-col gap-2 overflow-y-auto">
+                    {reservations.filter(step.filter).map((reservation) => {
+                      const reservationDate = parse(reservation.date, 'yyyy-MM-dd', new Date());
+                      const dDay = differenceInDays(reservationDate, now());
+
+                      return (
+                        <div
+                          key={reservation.reservationId}
+                          className={twMerge(
+                            'flex cursor-pointer flex-col gap-2 rounded-lg border border-gray-50 bg-gray-50 p-4 shadow',
+                            step.borderColor,
+                          )}
+                        >
+                          <section className="flex flex-col">
+                            <div className="flex justify-between">
+                              <p className="title1-nanum text-[22px]">
+                                {reservation.room.placeName} {reservation.room.unitNumber}
+                              </p>
+                              {step.renderBadge(reservations)}
+                            </div>
+                            <p className="caption1-nanum text-primary-gray-500">
+                              {format(reservationDate, 'yyyy년 MM월 dd일 EEEE', { locale: ko })}
+                            </p>
+                          </section>
+
+                          {dDay > 0 ? (
+                            <p className={twMerge('title1-nanum font-[26px]', step.textColor)}>
+                              D-{dDay}
+                            </p>
+                          ) : (
+                            <div className="h-7" />
+                          )}
+
+                          <div className="flex gap-2">
+                            <p className="text-sidebar-primary body1-pretendard rounded-sm bg-white p-2">
+                              {reservation.startTime}-{reservation.endTime}
+                            </p>
+                            <p className="text-sidebar-primary body1-pretendard rounded-sm bg-white p-2">
+                              {reservation.people}명
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            {reservations.length === 0 && (
+              <div className="my-auto flex w-fit items-center gap-20 self-center rounded-2xl bg-gray-50 py-4 pr-24 pl-4">
+                <Image
+                  src={EmptyReservationsImage}
+                  alt="empty-reservations"
+                  width={200}
+                  height={200}
+                />
+
+                <div className="flex flex-col gap-6">
+                  <div className="flex flex-col gap-2">
+                    <h2 className="title1-nanum text-zinc-800">예약한 강의실이 없습니다</h2>
+                    <h2 className="body1-nanum text-zinc-800">
+                      아직 예약 신청한 경험이 없네요.
+                      <br />
+                      예약하러 가볼까요?
+                    </h2>
+                  </div>
+                  <Link
+                    href="/"
+                    className="body2-nanum bg-classpick-500 w-fit rounded-full px-8 py-4 text-base text-white"
+                  >
+                    나에게 맞는 강의실 예약하러 가기
+                  </Link>
+                </div>
+              </div>
+            )}
+          </>
+        ) : null}
+      </div>
+    </div>
+  );
 }
